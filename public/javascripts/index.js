60fe41cbcd4615440fda2eb7d4d12db992f17ba6
salim <salimtison@gmail.Com>
2021-04-29 15:05:55 +0200

    testssssss

---
+++ public/javascripts/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    // const button = document.querySelector('button');
+    // button.addEventListener('click', () => {
+    //     console.log('test');
+    // })
+
+
+})
+
+
+const greating = (message) => {
+    console.log(message + ' bg');
+}
